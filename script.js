b3e8324f71d33dd28506260c69f5841dfc877464
Ohjay33 <61249083+Ohjay33@users.noreply.github.com>
2024-01-07 23:36:04 -0600

    commit1

---
+++ script.js
@@ -0,0 +1,25 @@
+function colorchange() {
+    var action = document.querySelector("body")
+    action.style.backgroundColor = "green"
+    console.log("you did it!");
+}
+
+
+// function randomColor() {
+//     return '#' + Math.random().toString(16).slice(2, 8);
+// };
+
+// $("#button").on("click", function () {
+//     $('body').css('background', randomColor());
+// });
+ 
+function setBg () {
+    var randomColor = Math.floor(Math.random()*16777215).toString(16);
+    document.body.style.backgroundColor = "#" + randomColor;
+    console.log("Done! You did it!!!");
+    // color.innerHTML = "#" + randomColor;
+}
+
+
+
+
